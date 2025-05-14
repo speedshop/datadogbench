03c2bbab1ac595879743970edd6efc4cabe2e58b
Nate Berkopec <nate.berkopec@gmail.com>
2025-05-14 12:57:19 +0900

    Updates

--- ddbench.rb
+++ ddbench.rb
@@ -1,10 +1,42 @@
 require 'benchmark/ips'
 require 'active_support/all'
-require 'datadog'
 
-# Configure Datadog in development mode
-Datadog.configure do |c|
-  c.tracing.instrument :active_support, enabled: true
+
+as_enabled = ENV['AS_ENABLED'] == 'true'
+dd_enabled = ENV['DD_ENABLED'] == 'true'
+dd_enabled = true if as_enabled
+
+if ENV['PATCH']
+  module Datadog
+    module Tracing
+      module Contrib
+        module ActiveSupport
+          module Cache
+            module Events
+              module OverrideAll
+                def all
+                  []
+                end
+              end
+
+              class << self
+                prepend OverrideAll
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+if dd_enabled
+  require 'datadog'
+
+  # Configure Datadog in development mode
+  Datadog.configure do |c|
+    c.tracing.instrument :active_support, enabled: as_enabled
+  end
 end
 
 # Create a memory store cache
@@ -21,4 +53,14 @@ Benchmark.ips do |x|
   end
 
   x.compare!
+end
+
+if ENV['FLAMEGRAPH']
+  require 'singed'
+  Singed.output_directory = "/tmp"
+  flamegraph(interval: 100) {
+    10_000.times do
+      cache.read(TEST_KEY)
+    end
+  }
 end
